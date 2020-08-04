ef65d933f66e24c75b99e2333d4c64537f576789
Kabu <t.kaburagi@me.com>
2020-08-04 11:22:02 +0900

    Delete table.sh

--- table.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-MYSQL_HOST=192.168.100.101
-
-mysql -h ${MYSQL_HOST} -u root -prooooot -D handson -e "drop table if exists animals"
-mysql -h ${MYSQL_HOST} -u root -prooooot -D handson -e "create table animals (animal varchar(100));"
